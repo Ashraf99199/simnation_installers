6689ea399a67d5d4a89322b39d6d8f156dabb2d5
hhk02 <80901734+hhk02@users.noreply.github.com>
2024-04-28 13:21:47 +0200

    Update simnation_mac.sh

--- simnation_mac.sh
+++ simnation_mac.sh
@@ -6,19 +6,6 @@ export SIMNATION_URL="http://simnation.ddns.net/patch.zip"
 export MONO_URL="https://beta.freeso.org/LauncherResourceCentral/Mono"
 export SDL_URL="https://beta.freeso.org/LauncherResourceCentral/SDL"
 
-DownloadSDL() {
-    if [ -f /usr/local/bin/curl ]; then
-        curl -L "$SDL_URL" --output sdl.dmg
-        sudo hdiutil attach sdl.dmg
-	if [ -f /Library/Frameworks/SDL2.framework ]; then
-        	sudo rm -rf /Library/Frameworks/SDL2.framework
-        fi
-	sudo cp -R /Volumes/SDL2/SDL2.framework /Library/Frameworks
-        sudo hdiutil unmount /Volumes/SDL2
-        DownloadTSO
-    fi
-}
-
 InstallSimNation () {
     echo "Downloading"
     mkdir -p "$HOME/Documents/SimNation/"
@@ -50,8 +37,21 @@ DownloadTSO () {
 	fi
 }
 
+DownloadSDL() {
+    if [ -f /usr/bin/curl ]; then
+        curl -L "$SDL_URL" --output sdl.dmg
+        sudo hdiutil attach sdl.dmg
+	if [ -f /Library/Frameworks/SDL2.framework ]; then
+        	sudo rm -rf /Library/Frameworks/SDL2.framework
+        fi
+	sudo cp -R /Volumes/SDL2/SDL2.framework /Library/Frameworks
+        sudo hdiutil unmount /Volumes/SDL2
+        DownloadTSO
+    fi
+}
+
 DownloadMono() {
-    if [ -f /usr/local/bin/curl ]; then
+    if [ -f /usr/bin/curl ]; then
         echo "Curl detected!"
         curl -L "$MONO_URL" --output mono.pkg
         echo "You need install this for run SimNation!"
@@ -59,7 +59,5 @@ DownloadMono() {
         DownloadSDL
     fi
 }
-
-
 
 DownloadMono
